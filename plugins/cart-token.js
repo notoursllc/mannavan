49c738c3b649a003d5985d1a70a3ee50f8a8899f
greg bruins <gregbruins@gmail.com>
2018-05-21 21:55:10 -0700

    debugging cart-token.js

--- plugins/cart-token.js
+++ plugins/cart-token.js
@@ -3,15 +3,18 @@ import shopping_cart_mixin from '@/mixins/shopping_cart_mixin';
 
 export default ({ $axios, store }) => {
     // TODO: store.state.shoppingcart.token is not set on www.gobreadvan.com
-    console.log("COOKIE DOMAIN?", store.state.shoppingcart.token);
     if(store.state.shoppingcart.token) {
         let domain = process.env.COOKIE_DOMAIN ? { domain: process.env.COOKIE_DOMAIN } : null;
         console.log("COOKIE DOMAIN", domain);
 
+        // Cookies.set(
+        //     'cart-jwt',
+        //     store.state.shoppingcart.token,
+        //     domain
+        // );
         Cookies.set(
             'cart-jwt',
-            store.state.shoppingcart.token,
-            domain
+            store.state.shoppingcart.token
         );
     }
 }
