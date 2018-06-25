1701ac056b9b61939b3ac75f21d57246a1ae6ee8
greg bruins <gregbruins@gmail.com>
2018-06-24 22:59:39 -0700

    adding some logging

--- server/plugins/hapi-basic-auth/index.js
+++ server/plugins/hapi-basic-auth/index.js
@@ -7,15 +7,19 @@ exports.plugin = {
         server.register(require('hapi-auth-basic'));
 
         const validate = async function (request, username, password, h) {
+            console.log("BASIC AUTH VALIDATE", username, password);
             if (!username
                 || !password
                 || username != process.env.API_USERNAME) {
+                    console.log("BASIC AUTH FAIL 1", username, password, process.env.API_USERNAME)
                 return { credentials: null, isValid: false };
             }
 
 
             const isValid = await Bcrypt.compare(password, process.env.API_PASSWORD_HASH);
             const credentials = { id: username };
+
+            console.log("BASIC AUTH IS VALID", isValid, password, process.env.API_PASSWORD_HASH)
 
             return { isValid, credentials };
         };
