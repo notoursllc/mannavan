08c58b189c3f0c6de450b9eb62c5172d22153b3d
greg bruins <gregbruins@gmail.com>
2018-08-30 21:51:30 -0700

    updating returns page

--- pages/returns/index.vue
+++ pages/returns/index.vue
@@ -29,13 +29,38 @@ export default {
 
 
 <template>
-    <div class="pageContainerMax tac">
-        {{ this.$t('returns_help')}}:
+    <div class="pageContainerMax">
+        <!-- {{ this.$t('returns_help')}}: -->
 
-        <div class="mts fs20">
-            <a :href="`mailto:${returnsEmailAddress}`">{{ returnsEmailAddress }}</a>
+        Items can be returned within 30 days of delivery as long as they are unwashed, unworn, and undamaged. Please note that shipping charges are non-refundable.
+
+        <div class="mtl">
+            <div class="fwb">To return an item:</div>
+
+            <ol class="mts orderedList">
+                <li>Include a copy of the packing slip or confirmation email from your order.</li>
+                <li>Mail your return to:<br/>
+                TBD Mailing address</li>
+                <li>We will issue your refund when the return is received</li>
+            </ol>
+
+            <div class="mtl">
+                <h2 class="fwb">Questions?</h2>
+                <div><a :href="`mailto:${returnsEmailAddress}`">{{ returnsEmailAddress }}</a></div>
+            </div>
         </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+.orderedList {
+    margin: 0 20px;
+
+    li {
+        padding-left: 10px;
+        margin-bottom: 10px;
+    }
+}
+</style>
 
 
