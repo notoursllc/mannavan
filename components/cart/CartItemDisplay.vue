a0a7322cb95c2a8646f82358928a1d825caffd53
greg bruins <gregbruins@gmail.com>
2018-10-03 22:26:21 -0700

    adding new component CartItemDisplay

---
+++ components/cart/CartItemDisplay.vue
@@ -0,0 +1,127 @@
+<script>
+export default {
+}
+</script>
+
+<template>
+    <article class="cartItem">
+
+        <!-- pic -->
+        <div class="picCell">
+            <slot name="pic"></slot>
+        </div>
+
+        <div class="flexGrow pam">
+            <div class="cartItemCols">
+                <!-- title -->
+                <div class="cartItemCol flexGrow">
+                    <slot name="title"></slot>
+                </div>
+
+                <!-- Variants -->
+                <div class="cartItemCol">
+                    <label>{{ $t('Size') }}:</label>
+                    <span>
+                        <slot name="size"></slot>
+                    </span>
+                </div>
+
+                <!-- Price -->
+                <div class="cartItemCol">
+                    <label>{{ $t('Price') }}:</label>
+                    <span>
+                        <slot name="price"></slot>
+                    </span>
+                </div>
+
+                <!-- Quantity -->
+                <div class="cartItemCol">
+                    <label>{{ $t('Quantity') }}:</label>
+                    <span>
+                        <slot name="quantity"></slot>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </article>
+</template>
+
+<style lang="scss" scoped>
+    @import "~assets/css/components/_variables.scss";
+    @import "~assets/css/components/_mixins.scss";
+
+    .cartItem {
+        width: 100%;
+        margin-bottom: 20px;
+        background-color: #fff;
+        @include box-shadow(0px, 1px, 2px, rgba(0,0,0,.1));
+        transition: background-color .5s linear;
+        @include flexbox();
+        @include flex-wrap(nowrap);
+        @include flex-direction(row);
+        @include align-content(stretch);
+
+        .cartItemCols {
+            @include flexbox();
+            @include flex-wrap(nowrap);
+            @include flex-direction(row);
+            @include align-content(stretch);
+
+            .cartItemCol {
+                padding: 0 15px;
+
+                >label,
+                >span {
+                    display: block;
+                }
+
+                >label {
+                    font-size: 12px;
+                    color: #7e7e7e;
+                }
+            }
+        }
+
+        .picCell {
+            width: 128px;
+            position: relative;
+        }
+
+        .cartItemPic {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            background-size: cover;
+            background-position: center;
+        }
+    }
+
+    @media #{$medium-and-down} {
+        .cartItemCols {
+            @include flex-direction(column !important);
+            @include align-items(flex-start !important);
+
+            .cartItemCol {
+                margin-bottom: 5px;
+                display: table-row !important;
+
+                >label,
+                >span {
+                    display: inline-block !important;
+                }
+
+                >label {
+                    padding-right: 10px;
+                    vertical-align: top;
+                }
+            }
+        }
+    }
+
+    .cartItem.highlight {
+        background-color: #d2f1d3;
+    }
+    .cartItem.fadeout {
+        background-color: #fff;
+    }
+</style>
--- components/cart/CartItems.vue
+++ components/cart/CartItems.vue
@@ -1,123 +1,125 @@
 <script>
-    import accounting from 'accounting'
-    import Vue from 'vue'
-    import { mapGetters } from 'vuex'
-    import isObject from 'lodash.isobject'
-    import { Select, Option, InputNumber, Loading, Button, Popover, Notification } from 'element-ui'
-    import ProductPrice from '@/components/product/ProductPrice'
-    import NumberButtons from '@/components/NumberButtons'
-    import product_mixin from '@/mixins/product_mixin'
-    import shopping_cart_mixin from '@/mixins/shopping_cart_mixin'
+import accounting from 'accounting'
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+import isObject from 'lodash.isobject'
+import { Select, Option, InputNumber, Loading, Button, Popover, Notification } from 'element-ui'
+import ProductPrice from '@/components/product/ProductPrice'
+import NumberButtons from '@/components/NumberButtons'
+import CartItemDisplay from '@/components/cart/CartItemDisplay'
+import product_mixin from '@/mixins/product_mixin'
+import shopping_cart_mixin from '@/mixins/shopping_cart_mixin'
 
-    Vue.use(Select);
-    Vue.use(Option);
-    Vue.use(InputNumber);
-    Vue.use(Button);
-    Vue.use(Popover);
-    Vue.use(Loading.directive);
+Vue.use(Select);
+Vue.use(Option);
+Vue.use(InputNumber);
+Vue.use(Button);
+Vue.use(Popover);
+Vue.use(Loading.directive);
 
-    Vue.prototype.$notify = Notification;
-    let currentNotification = null;
+Vue.prototype.$notify = Notification;
+let currentNotification = null;
 
 
-    function showNotification(Notification) {
-        if(currentNotification) {
-            currentNotification.close();
-        }
-        currentNotification = Notification
+function showNotification(Notification) {
+    if(currentNotification) {
+        currentNotification.close();
     }
+    currentNotification = Notification
+}
 
 
-    export default {
-        props: {
-            allowEdit: {
-                type: Boolean,
-                default: true
-            },
-
-            highlightItem: {
-                type: String,
-                default: null
-            }
+export default {
+    props: {
+        allowEdit: {
+            type: Boolean,
+            default: true
         },
 
-        components: {
-            ProductPrice,
-            NumberButtons
-        },
-
-        mixins: [
-            product_mixin,
-            shopping_cart_mixin
-        ],
-
-        data() {
-            return {
-                added_cart_item: {},
-                selectedQty: 0,
-                loading: true,
-                confirmDeleteModals: {}
-            }
-        },
-
-        computed: {
-            ...mapGetters({
-                shoppingCart: 'shoppingcart/cart'
-            })
-        },
-
-        methods: {
-            async updateCartItemQuantity(item, qty) {
-                try {
-                    const loadingInstance = Loading.service({ target: `#cartItem${item.id}` });
-
-                    const response = await this.updateItemQty({
-                        id: item.id,
-                        qty
-                    });
-                    this.setCartAndTokenStateFromResponse(response);
-
-                    item.qty = qty;
-                    loadingInstance.close();
-                }
-                catch(err) {
-                    showNotification(
-                        this.$notify({
-                            type: 'error',
-                            title: this.$t('An error occurred'),
-                            duration: 0
-                        })
-                    )
-                }
-            },
-
-            async removeItem(id) {
-                try {
-                    const loadingInstance = Loading.service({ target: `#cartItem${id}` });
-
-                    const response = await this.deleteItem({ id });
-                    this.setCartAndTokenStateFromResponse(response);
-
-                    loadingInstance.close();
-                }
-                catch(err) {
-                    showNotification(
-                        this.$notify({
-                            type: 'error',
-                            title: this.$t('An error occurred'),
-                            duration: 0
-                        })
-                    )
-                }
-            }
-        },
-
-        mounted() {
-            setTimeout(() => {
-                this.added_cart_item = this.highlightItem;
-            }, 1000)
+        highlightItem: {
+            type: String,
+            default: null
         }
+    },
+
+    components: {
+        ProductPrice,
+        NumberButtons,
+        CartItemDisplay
+    },
+
+    mixins: [
+        product_mixin,
+        shopping_cart_mixin
+    ],
+
+    data() {
+        return {
+            added_cart_item: {},
+            selectedQty: 0,
+            loading: true,
+            confirmDeleteModals: {}
+        }
+    },
+
+    computed: {
+        ...mapGetters({
+            shoppingCart: 'shoppingcart/cart'
+        })
+    },
+
+    methods: {
+        async updateCartItemQuantity(item, qty) {
+            try {
+                const loadingInstance = Loading.service({ target: `#cartItem${item.id}` });
+
+                const response = await this.updateItemQty({
+                    id: item.id,
+                    qty
+                });
+                this.setCartAndTokenStateFromResponse(response);
+
+                item.qty = qty;
+                loadingInstance.close();
+            }
+            catch(err) {
+                showNotification(
+                    this.$notify({
+                        type: 'error',
+                        title: this.$t('An error occurred'),
+                        duration: 0
+                    })
+                )
+            }
+        },
+
+        async removeItem(id) {
+            try {
+                const loadingInstance = Loading.service({ target: `#cartItem${id}` });
+
+                const response = await this.deleteItem({ id });
+                this.setCartAndTokenStateFromResponse(response);
+
+                loadingInstance.close();
+            }
+            catch(err) {
+                showNotification(
+                    this.$notify({
+                        type: 'error',
+                        title: this.$t('An error occurred'),
+                        duration: 0
+                    })
+                )
+            }
+        }
+    },
+
+    mounted() {
+        setTimeout(() => {
+            this.added_cart_item = this.highlightItem;
+        }, 1000)
     }
+}
 </script>
 
 
@@ -127,84 +129,77 @@
             {{ $t('Your shopping cart does not contain any items.') }}
         </div>
         <div v-else class="ptl">
-            <article v-for="item in shoppingCart.cart_items"
-                    :key="item.id"
-                    class="cartItem"
-                    :class="{'highlight': highlightItem === item.id, 'fadeout': added_cart_item === item.id}"
-                    :id="'cartItem' + item.id">
-                <div class="cartItemPic" :style="'background-image:url(' + featuredProductPic(item.product) + ');'">
-                    <figure class="image"></figure>
-                </div>
-                <!-- <div class="cartItemPicMobile" :style="'background-image:url(' + featuredProductPic(item.product) + ');'">foo</div> -->
+            <cart-item-display
+                v-for="item in shoppingCart.cart_items"
+                :key="item.id"
+                :class="{'highlight': highlightItem === item.id, 'fadeout': added_cart_item === item.id}"
+                :id="'cartItem' + item.id">
 
-                <div class="cartItemInfo">
-                    <div class="cartItemInfoContent">
-                        <div class="cartItemMain">
-                            <nuxt-link :to="{ name: 'type-name-seouri', params: { seouri: item.product.seo_uri} }"
-                                tag="a"
-                                class="itemTitle">{{ item.product.title }}</nuxt-link>
+                <!-- pic -->
+                <template slot="pic">
+                    <figure class="cartItemPic" :style="'background-image:url(' + featuredProductPic(item.product) + ');'"></figure>
+                </template>
 
-                            <div v-if="allowEdit" class="mts">
-                                <el-popover
-                                    width="200"
-                                    v-model="confirmDeleteModals[item.id]"
-                                    trigger="click"
-                                    placement="start/right-end">
-                                    <div>{{ $t('Delete this item?') }}</div>
-                                    <div class="tar mtm">
-                                        <el-button
-                                            type="primary"
-                                            size="mini"
-                                            @click="confirmDeleteModals[item.id] = false; removeItem(item.id)">{{ $t('CONFIRM') }}</el-button>
+                <!-- title -->
+                <template slot="title">
+                    <nuxt-link :to="{ name: 'type-name-seouri', params: { seouri: item.product.seo_uri} }"
+                            tag="a"
+                            class="itemTitle">{{ item.product.title }}</nuxt-link>
 
-                                        <el-button
-                                            size="mini"
-                                            type="text"
-                                            @click="confirmDeleteModals[item.id] = false">{{ $t('cancel') }}</el-button>
-                                    </div>
-                                    <el-button
-                                        slot="reference"
-                                        type="text"
-                                        @click="confirmDeleteModals[item.id] = true">{{ $t('Delete') }}</el-button>
-                                </el-popover>
+                    <div v-if="allowEdit" class="mts">
+                        <el-popover
+                            width="200"
+                            v-model="confirmDeleteModals[item.id]"
+                            trigger="click"
+                            placement="start/right-end">
+                            <div>{{ $t('Delete this item?') }}</div>
+                            <div class="tar mtm">
+                                <el-button
+                                    type="primary"
+                                    size="mini"
+                                    @click="confirmDeleteModals[item.id] = false; removeItem(item.id)">{{ $t('CONFIRM') }}</el-button>
+
+                                <el-button
+                                    size="mini"
+                                    type="text"
+                                    @click="confirmDeleteModals[item.id] = false">{{ $t('cancel') }}</el-button>
                             </div>
-                        </div>
+                            <el-button
+                                slot="reference"
+                                type="text"
+                                @click="confirmDeleteModals[item.id] = true">{{ $t('Delete') }}</el-button>
+                        </el-popover>
+                    </div>
+                </template>
 
-                        <!-- Variants -->
-                        <div class="cartItemCol">
-                            <div v-if="item.variants && item.variants.size">
-                                <label class="itemLabel">{{ $t('Size') }}:</label>
-                                <div class="itemVal">{{ $t(item.variants.size) }}</div>
-                            </div>
-                        </div>
+                <!-- size -->
+                <template slot="size">
+                    {{ $t(item.variants.size) }}
+                </template>
 
-                        <!-- Price -->
-                        <div class="cartItemCol">
-                            <label class="itemLabel">{{ $t('Price' )}}:</label>
-                            <div class="itemVal"><product-price :product="item.product"></product-price></div>
-                        </div>
+                <!-- price -->
+                <template slot="price">
+                    <product-price :product="item.product" />
+                </template>
 
-                        <!-- Quantity -->
-                        <div class="cartItemCol">
-                            <label class="itemLabel">{{ $t('Quantity' )}}:</label>
-                            <div v-if="allowEdit" class="itemVal">
-                                <div class="displayTableCell prl fwb vat">{{ item.qty }}</div>
-                                <div class="displayTableCell">
-                                    <number-buttons :step="1"
-                                                    :min="1"
-                                                    :max="item.product.inventory_count"
-                                                    :init-value="item.qty"
-                                                    size="small"
-                                                    v-on:change="val => {updateCartItemQuantity(item, val)}"></number-buttons>
-                                </div>
-                            </div>
-                            <div v-else class="itemVal">
-                                {{ item.qty }}
-                            </div>
+                <!-- quantity -->
+                <template slot="quantity">
+                    <div v-if="allowEdit" class="itemVal">
+                        <div class="displayTableCell prl fwb vat">{{ item.qty }}</div>
+                        <div class="displayTableCell">
+                            <number-buttons :step="1"
+                                            :min="1"
+                                            :max="item.product.inventory_count"
+                                            :init-value="item.qty"
+                                            size="small"
+                                            v-on:change="val => {updateCartItemQuantity(item, val)}"></number-buttons>
                         </div>
                     </div>
-                </div>
-            </article>
+                    <div v-else class="itemVal">
+                        {{ item.qty }}
+                    </div>
+                </template>
+            </cart-item-display>
         </div>
     </div>
 </template>
