47286fa449474c090994282dfe44e6b3608d2832
greg bruins <gregbruins@gmail.com>
2019-02-09 16:14:35 -0800

    updating bugsnag libs

--- plugins/bugsnag.js
+++ plugins/bugsnag.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import bugsnag from 'bugsnag-js'
-import bugsnagVue from 'bugsnag-vue'
+import bugsnag from '@bugsnag/js'
+import bugsnagVue from '@bugsnag/plugin-vue'
 
 const bugsnagClient = bugsnag({
-  apiKey: process.env.BUG_SNAG_API_KEY,
-  notifyReleaseStages: [ 'production' ]
+    apiKey: process.env.BUG_SNAG_API_KEY,
+    notifyReleaseStages: [ 'production' ]
 });
 
-bugsnagClient.use(
-    bugsnagVue(Vue)
-);
+bugsnagClient.use(bugsnagVue, Vue);
