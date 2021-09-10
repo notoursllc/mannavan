e5ea70fd13a89e71daaa3724ce9a2760f2dd5828
gbru <gbru@users.noreply.github.com>
2021-09-09 20:51:46 -0700

    fix: Add missing content path in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 // https://tailwindcss.nuxtjs.org/tailwind-config/#default-configuration
 const figleafTailwindConfig = require('@notoursllc/figleaf/tailwind.config.js');
 
+// figleafTailwindConfig.purge.enabled = true;
+
 // Add our own paths:
 figleafTailwindConfig.purge.content.push(
-    'components/**/*.vue', // this is already defined in figleaf
+    'components/**/*.vue',
     'layouts/**/*.vue',
     'pages/**/*.vue',
     'plugins/**/*.js',
     'nuxt.config.js',
-    'node_modules/@notoursllc/figleaf/components/**/*.vue'
+    'node_modules/@notoursllc/figleaf/components/**/*.vue',
+    'node_modules/@notoursllc/figleaf/layouts/**/*.vue'
 );
 
 figleafTailwindConfig.jit = true;
