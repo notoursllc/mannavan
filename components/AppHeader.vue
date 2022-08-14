544a89ce25cbe096b3e9b5800ddb75b505d267ea
gbru <gbru@users.noreply.github.com>
2022-08-13 22:17:15 -0700

    Add CurrencyChooser component and update AppHeader

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,21 +1,15 @@
 <script>
+import CurrencyChooser from '@/components/currency/currencyChooser/CurrencyChooser.vue';
 import {
     FigHeader,
-    FigVictoryIcon,
-    FigDropdown,
-    FigDropdownButton,
-    FigButton,
-    FigCurrencySymbol
+    FigVictoryIcon
 } from '@notoursllc/figleaf';
 
 export default {
     components: {
+        CurrencyChooser,
         FigHeader,
-        FigVictoryIcon,
-        FigDropdown,
-        FigDropdownButton,
-        FigButton,
-        FigCurrencySymbol
+        FigVictoryIcon
     },
 
     computed: {
@@ -29,26 +23,6 @@ export default {
 
         numCartItems() {
             return this.$store.state.cart.num_items;
-        },
-
-        cartCurrency() {
-            return this.$store.state.cart.currency || this.defaultExchangeRate;
-        },
-
-        defaultExchangeRate() {
-            return this.$store.state.ui.exchangeRates?.default || 'USD';
-        },
-
-        exchangeRates() {
-            const rates = this.$store.state.ui.exchangeRates?.rates || {};
-            let filteredRates = {};
-
-            if(rates) {
-                filteredRates = { ...rates };
-                delete filteredRates[this.cartCurrency];
-            }
-
-            return Object.keys(filteredRates);
         }
     },
 
@@ -63,31 +37,6 @@ export default {
 
         onLogoClick() {
             this.$router.push({ name: 'index' });
-        },
-
-        async onExchangeRateClick(val) {
-            if(this.exchangeRates.includes(val)) {
-                this.$store.dispatch('cart/CART_CURRENCY', val);
-
-                const cartId = this.$store.state.cart.id;
-
-                if(cartId) {
-                    try {
-                        const { data } = await this.$api.cart.currency({
-                            id: cartId,
-                            currency: val
-                        });
-
-                        await Promise.all([
-                            this.$store.dispatch('cart/CART', data.cart),
-                            this.$store.dispatch('ui/EXCHANGE_RATES', data.exchange_rates)
-                        ]);
-                    }
-                    catch(err) {
-                        // no action
-                    }
-                }
-            }
         }
     }
 };
@@ -99,29 +48,6 @@ export default {
         :num-cart-items="numCartItems"
         @cartClick="onCartButtonClick"
         @sidebarOpen="onSidebarOpen">
-        <template v-slot:top>
-            <!-- currency chooser -->
-            <div class="flex px-2 py-2">
-                <div class="flex-grow"></div>
-                <div>
-                    <fig-dropdown>
-                        <template v-slot:toggler>
-                            <fig-button variant="naked">
-                                <fig-currency-symbol :currency="cartCurrency" /> {{ cartCurrency }}
-                            </fig-button>
-                        </template>
-
-                        <fig-dropdown-button
-                            v-for="(rate, idx) in exchangeRates"
-                            :key="idx"
-                            @click="onExchangeRateClick(rate)">
-                            <fig-currency-symbol :currency="rate" /> {{ rate }}
-                        </fig-dropdown-button>
-                    </fig-dropdown>
-                </div>
-            </div>
-        </template>
-
         <template v-slot:logo>
             <div @click="onLogoClick">
                 <fig-victory-icon
@@ -143,6 +69,16 @@ export default {
                     active-class="active">{{ $t(type) }}</nuxt-link>
             </nav>
         </template>
+
+        <template v-slot:top>
+            <!-- currency chooser -->
+            <div class="flex px-2 py-2">
+                <div class="flex-grow"></div>
+                <div>
+                    <currency-chooser />
+                </div>
+            </div>
+        </template>
     </fig-header>
 </template>
 
@@ -151,16 +87,7 @@ export default {
 .fig-header {
     @apply bg-white border-b border-gray-300;
 }
-.fig-header:after {
-    /* background: linear-gradient(to right, #e14e2c 10%, #eb7d32 21%, #eb7d32 56%, #efc243 67%, #efc243 100%); */
-    /* background: linear-gradient(to right, #e14e2c 30%, #eb7d32 31%, #eb7d32 66%, #efc243 67%, #efc243 100%); */
-    /* background: linear-gradient(to right, #7ec3d4 30% ,#cceaf1 31%, #cceaf1 66%, #7ec3d4 67%, #7ec3d4 100%); */
-    /* background: #9cdff0; */
-    /* background: #f9c19b; */
-    content: '';
-    height: 1px;
-    @apply right-0 left-0 top-0 absolute;
-}
+
 .fig-header nav a {
     @apply px-2 py-1 rounded text-gray-700 border-transparent;
 }
---
+++ components/currency/currencyChooser/CurrencyChooser.vue
@@ -0,0 +1,105 @@
+<script>
+import {
+    FigDropdown,
+    FigDropdownButton,
+    FigButton,
+    FigCurrencySymbol,
+    FigUseTime,
+    FigUseCountry
+} from '@notoursllc/figleaf';
+
+
+export default {
+    components: {
+        FigDropdown,
+        FigDropdownButton,
+        FigButton,
+        FigCurrencySymbol
+    },
+
+    computed: {
+        cartCurrency() {
+            return this.$store.state.cart.currency || this.defaultExchangeRate;
+        },
+
+        defaultExchangeRate() {
+            return this.$store.state.ui.exchangeRates?.default || 'USD';
+        },
+
+        exchangeRates() {
+            const rates = this.$store.state.ui.exchangeRates?.rates || {};
+            let filteredRates = {};
+
+            if(rates) {
+                filteredRates = { ...rates };
+                delete filteredRates[this.cartCurrency];
+            }
+
+            return Object.keys(filteredRates);
+        }
+    },
+
+    methods: {
+        async onExchangeRateClick(val) {
+            if(this.exchangeRates.includes(val)) {
+                this.$store.dispatch('cart/CART_CURRENCY', val);
+
+                const cartId = this.$store.state.cart.id;
+
+                if(cartId) {
+                    try {
+                        const { data } = await this.$api.cart.currency({
+                            id: cartId,
+                            currency: val
+                        });
+
+                        await Promise.all([
+                            this.$store.dispatch('cart/CART', data.cart),
+                            this.$store.dispatch('ui/EXCHANGE_RATES', data.exchange_rates)
+                        ]);
+                    }
+                    catch(err) {
+                        // no action
+                    }
+                }
+            }
+        }
+    },
+
+    mounted() {
+        // Set the cart currency to the currency based on the browser's timezone
+        // if the cart currency is not already set
+        if(!this.$store.state.cart.currency) {
+            const { getCountryFromTimezone } = FigUseTime();
+            const { getCountryCodeToCurrencyCodeMap } = FigUseCountry();
+
+            const tz = getCountryFromTimezone();
+            const countryToCurrencyMap = getCountryCodeToCurrencyCodeMap();
+            const currency = countryToCurrencyMap[tz];
+            const rates = this.$store.state.ui.exchangeRates?.rates || {};
+
+            if(currency && rates[currency]) {
+                this.$store.dispatch('cart/CART_CURRENCY', currency);
+            }
+        }
+    }
+};
+</script>
+
+<template>
+    <fig-dropdown>
+        <template v-slot:toggler>
+            <fig-button variant="naked">
+                <fig-currency-symbol :currency="cartCurrency" /> {{ cartCurrency }}
+            </fig-button>
+        </template>
+
+        <fig-dropdown-button
+            v-for="(rate, idx) in exchangeRates"
+            :key="idx"
+            @click="onExchangeRateClick(rate)">
+            <fig-currency-symbol :currency="rate" /> {{ rate }}
+        </fig-dropdown-button>
+    </fig-dropdown>
+</template>
+
